99a8c19a5327ed008474211d08db8a663351b351
Tushar Mangla <tushar.mangla1120@gmail.com>
2023-08-12 19:08:16 +0530

    remove

--- E-School-master/CRUD-APP-1/src/compnenets/index.js
+++
@@ -1,4 +0,0 @@
-export { default as FormRow } from './FormRow';
-export { default as Sidebar } from './Sidebar';
-export { default as Navbar } from './Navbar';
-export { default as Auth } from './Auth';
--- E-School-master/CRUD-APP-1/src/redux/store.jsx
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import studentReducer from './studentSlice'
-
-const store = configureStore({
-    reducer: {
-        students: studentReducer
-    }
-})
-export default store;
